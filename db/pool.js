4a4f996975e068a04db938944b688206c0af57c6
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-06-08 03:35:52 +0200

    Add ability to log sql queries

--- db/pool.js
+++ db/pool.js
@@ -1,3 +1,17 @@
 const { Pool } = require("pg")
 
+if (process.env.LOG_QUERIES == true) {
+    const Query = require("pg").Query
+    const submit = Query.prototype.submit
+    Query.prototype.submit = function () {
+        const text = this.text
+        const values = this.values || []
+        const query = text.replace(/\$([0-9]+)/g, (m, v) =>
+            JSON.stringify(values[parseInt(v) - 1])
+        )
+        console.log(query)
+        submit.apply(this, arguments)
+    }
+}
+
 module.exports = new Pool()
