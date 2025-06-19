d9f9f8d3ee77883a1e1d3945a2f13639674b63e5
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-06-19 22:33:20 +0200

    Fix broken references

--- utils/utils.js
+++ utils/utils.js
@@ -1,13 +1,13 @@
 const path = require("path")
 
 exports.convertConstructorLogoPath = (constructor) => {
-    if (constructor.logo_path.includes(process.env.CONSTRUCTORS_IMAGE_PATH)) {
+    if (constructor.logoPath.includes(process.env.CONSTRUCTORS_IMAGE_PATH)) {
         return constructor
     }
 
-    constructor.logo_path = path.join(
+    constructor.logoPath = path.join(
         process.env.CONSTRUCTORS_IMAGE_PATH,
-        constructor.logo_path
+        constructor.logoPath
     )
 
     return constructor
