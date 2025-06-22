283534f51ebbfc4c22d732c6e885799104073550
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-06-23 01:30:35 +0200

    fix trim routes controller names

--- routes/trims.js
+++ routes/trims.js
@@ -1,14 +1,14 @@
 var express = require("express")
 const {
     postNewCarTrim,
-    updateCarTrim: putUpdateCarTrim,
+    updateCarTrim,
     deleteCarTrim,
 } = require("../controllers/carsController")
 var router = express.Router()
 
 // Trims
 router.post("/add", postNewCarTrim)
-router.post("/edit/:trimId", putUpdateCarTrim)
+router.post("/edit/:trimId", updateCarTrim)
 router.post("/delete/:trimId", deleteCarTrim)
 
 module.exports = router
