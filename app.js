616a3d266c5a7441427d08a2af673359136b3227
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-07-05 03:53:23 +0200

    Style error page

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message
+    res.locals.status = err.status || 500
     res.locals.error = req.app.get("env") === "development" ? err : {}
 
     // render the error page
